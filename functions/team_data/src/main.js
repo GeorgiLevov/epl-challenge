feeb6634f298db37dd1899ab09fd05efff4c14f4
GeorgiLevov <georgilevov@pm.me>
2024-09-27 16:32:39 -0700

    udpated node function

--- functions/team_data/src/main.js
+++ functions/team_data/src/main.js
@@ -4,27 +4,27 @@ import { Client, Users } from 'node-appwrite';
 export default async ({ req, res, log, error }) => {
   // You can use the Appwrite SDK to interact with other services
   // For this example, we're using the Users service
-  const client = new Client()
-    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers['x-appwrite-key'] ?? '');
-  const users = new Users(client);
+//   const client = new Client()
+//     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
+//     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+//     .setKey(req.headers['x-appwrite-key'] ?? '');
+//   const users = new Users(client);
 
-  try {
-    const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-  } catch(err) {
-    error("Could not list users: " + err.message);
-  }
+//   try {
+//     const response = await users.list();
+//     // Log messages and errors to the Appwrite Console
+//     // These logs won't be seen by your end users
+//     log(`Total users: ${response.total}`);
+//   } catch(err) {
+//     error("Could not list users: " + err.message);
+//   }
 
-  // The req object contains the request data
-  if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return res.text("Pong");
-  }
+//   // The req object contains the request data
+//   if (req.path === "/ping") {
+//     // Use res object to respond with text(), json(), or binary()
+//     // Don't forget to return a response!
+    return res.text("Yo, you called?");
+//   }
 
   return res.json({
     motto: "Build like a team of hundreds_",
