16c69479a7509558798f6986653762f0d092ead3
GeorgiLevov <georgilevov@pm.me>
2024-11-05 16:08:23 -0700

    Small test to log

--- functions/standings/src/main.js
+++ functions/standings/src/main.js
@@ -33,6 +33,10 @@ export default async ({ req, res, log, error }) => {
 
   const teamStandings = await getSTANDINGS();
 
+  log("logging...");
+  log(API_CONFIG.EPL_API_STANDINGS_PATH);
+  log(COLLECTIONS_CONFIG.EPL_PROJECT_STANDINGS_COLLECTION);
+
   if (Object.keys(teamStandings).length > 0) {
     
     const updateStandingsData = db.updateDocument(
